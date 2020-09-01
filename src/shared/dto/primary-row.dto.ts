4c5e62007a6c51153490bfe2f307aef8f2cc84c9
Demidov <demidov@yandex.ru>
2020-09-01 23:53:03 +0300

    add UserEntity

--- src/shared/dto/primary-row.dto.ts
+++ src/shared/dto/primary-row.dto.ts
@@ -1,18 +1,16 @@
-import {ApiModelProperty} from '@nestjs/swagger';
+
 import {Type} from 'class-transformer';
 import {CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm';
 
 export class PrimaryRowDto {
   @PrimaryGeneratedColumn()
-  @ApiModelProperty({type: Number, description: 'Идентификатор'})
   readonly id: number;
 
   @CreateDateColumn({type: 'timestamp'})
-  @ApiModelProperty({type: String, format: 'date-time', description: 'Дата создания записи'})
   @Type(() => Date)
   readonly createdAt: Date;
 
-  @ApiModelProperty({type: String, format: 'date-time', description: 'Дата последнего обновления записи'})
+
   @UpdateDateColumn({type: 'timestamp'})
   @Type(() => Date)
   readonly updatedAt: Date;
