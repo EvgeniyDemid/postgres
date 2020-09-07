6101063d563a4ecab836b7a69849594e570092a1
Demidov <demidov@yandex.ru>
2020-09-07 23:27:54 +0300

    add user create dto and user respons dto

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './userDto/createUserDto';
+import { ResponsUserDto } from './userDto/responsUserDto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(UserEntity) private contentRepository: Repository<UserEntity>){}
+
+    async create(createUserDto:CreateUserDto):Promise<ResponsUserDto>{
+        const { password, email, ...rest } = createUserDto;
+        const userByEmail = await this.contentRepository.findOne({where:{email}})
+        if (userByEmail) {
+          throw new HttpException(
+            'Пользователь с таким email уже существет ',
+            HttpStatus.CONFLICT,
+          );
+        }
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(password, salt);
+        const createUser = new this.UserModel(
+          _.assignIn(createUserDro, { password: hash, role }),
+        );
+        await createUser.save();  
+    }
+}
